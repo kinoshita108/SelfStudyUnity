368b16deb271d997be0f36fa5e135a2bda8fd54f
kinoshita <g2127021@st.t-kougei.ac.jp>
2024-04-26 18:41:08 +0900

    テスト

--- BuildScript/BuildScript.bat
+++ BuildScript/BuildScript.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Unity 2021.3.23f1\Editor\Unity.exe" -batchmode -projectPath "D:\SelfStudyUnity\SelfStudyUnity" -buildTarget Windows64 -executeMethod BuildScript.AutoBuild
+"C:\Program Files\Unity 2021.3.23f1\Editor\Unity.exe" -batchmode -projectPath "D:\SelfStudyUnity\SelfStudyUnity" -buildTarget Win64 -executeMethod BuildScript.AutoBuild
